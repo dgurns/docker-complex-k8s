4f3b2abb44426cfd2256a675135ab162a10e9083
Dan Gurney <dgurney@cartrawler.com>
2019-08-30 16:23:53 +0100

    Thru 158. Add logo back in.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import logo from './logo.svg';
 import './App.css';
 import OtherPage from './OtherPage';
 import Fib from './Fib';
@@ -9,6 +10,7 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
