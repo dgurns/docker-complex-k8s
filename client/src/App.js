34c6cd7983d7de4a07c95f6e1e3f7fd3ed44691b
Dan Gurney <dgurney@cartrawler.com>
2019-09-04 11:34:50 +0100

    Update text on homepage

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <p>Hurr I'm an app</p>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
